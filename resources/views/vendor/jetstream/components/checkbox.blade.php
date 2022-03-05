d05294658108e8ac506b48564a5240d3b586fd3c
Laurent Sabatier <48389949+githesix@users.noreply.github.com>
2022-03-05 12:10:36 +0100

    Jetstream components improvements

--- resources/views/vendor/jetstream/components/checkbox.blade.php
+++ resources/views/vendor/jetstream/components/checkbox.blade.php
@@ -1,1 +1,3 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-primary shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50']) !!}>
+@props(['disabled' => false])
+
+<input {{ $disabled ? 'disabled' : '' }} type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-primary shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50']) !!}>
