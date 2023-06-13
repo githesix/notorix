22accce252ac3ab06bb0ee1b60813cd6e163f985
Laurent Sabatier <48389949+githesix@users.noreply.github.com>
2023-06-13 17:35:47 +0200

    Correct SAML attributes

--- app/Listeners/SamlAssertionAttributes.php
+++ app/Listeners/SamlAssertionAttributes.php
@@ -11,10 +11,10 @@ class SamlAssertionAttributes
     public function handle(Assertion $event)
     {
         $event->attribute_statement
-        ->addAttribute(new Attribute(ClaimTypes::UID, auth()->user()->uid))
+        ->addAttribute(new Attribute(ClaimTypes::PPID, auth()->user()->uid))
         ->addAttribute(new Attribute(ClaimTypes::NAME, auth()->user()->name))
-        ->addAttribute(new Attribute(ClaimTypes::FIRSTNAME, auth()->user()->prenom))
-        ->addAttribute(new Attribute(ClaimTypes::LASTNAME, auth()->user()->nom))
+        ->addAttribute(new Attribute(ClaimTypes::GIVEN_NAME, auth()->user()->prenom))
+        ->addAttribute(new Attribute(ClaimTypes::COMMON_NAME, auth()->user()->nom))
         ->addAttribute(new Attribute(ClaimTypes::ROLE, auth()->user()->samlrole));
     }
 }
